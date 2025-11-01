6a9591cc6332134096ac11a3a16daf030e819312
x-Sambhav-x <nikhrasambhav@gmail.com>
2025-11-01 19:41:12 +0530

    Create Todo model with schema for MongoDB

---
+++ backend/backend/models/Todo.js
@@ -0,0 +1,8 @@
+const mongoose = require("mongoose");
+
+const TodoSchema = new mongoose.Schema({
+  text: { type: String, required: true },
+  completed: { type: Boolean, default: false }
+});
+
+module.exports = mongoose.model("Todo", TodoSchema);
