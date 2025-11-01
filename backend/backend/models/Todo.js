961b79cb0d2969452247b8212c299f381b25af4c
x-Sambhav-x <nikhrasambhav@gmail.com>
2025-11-01 23:46:09 +0530

    Delete backend directory

--- backend/backend/models/Todo.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const TodoSchema = new mongoose.Schema({
-  text: { type: String, required: true },
-  completed: { type: Boolean, default: false }
-});
-
-module.exports = mongoose.model("Todo", TodoSchema);
